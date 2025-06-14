18ef23799b6a0915c71944f74e9ec495baa8ed64
shou xiang <weeshouxiang@gmail.com>
2025-06-14 18:39:32 +0800

    added signs function

--- ft_signs.c
+++ ft_signs.c
@@ -1,22 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   ft_signs.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: wshou-xi <wshou-xi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/14 16:18:44 by wshou-xi          #+#    #+#             */
-/*   Updated: 2025/06/14 18:32:23 by wshou-xi         ###   ########.fr       */
+/*   Created: 2025/06/14 18:32:43 by wshou-xi          #+#    #+#             */
+/*   Updated: 2025/06/14 18:39:01 by wshou-xi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_printf(const char *format, ...)
+int	ft_signs(char signs, char *strings)
 {
-	va_list(args);
-	va_start(args,format);
-	
-	va_end(args);
-	return (0);	
+	if (!signs)
+		return (0);
+	if (signs == 'c')
+		return (ft_putstr(strings));
+	if (signs == 'd' || signs == 'i')
+		return (ft_putint(strings));
+	if (signs == 'u')
+		return (ft_putdec(strings));
+	if (signs == 'x')
+		return (ft_putlowhex(strings));
+	if (signs == 'X')
+		return (ft_putuphex(strings));
+	if (signs == '%')
+		return (ft_printpercent());
 }
